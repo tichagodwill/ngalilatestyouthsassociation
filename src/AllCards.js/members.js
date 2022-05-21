3f170b0ec2a038711dd1a500e2dc1c302c81a34c
Ticha <tichagodwill1@gmail.com>
2022-05-21 20:33:15 +0300

    changes

--- src/AllCards.js/members.js
+++ src/AllCards.js/members.js
@@ -1,105 +1,93 @@
-export const members =[
-    {
-        id: 1,
-        image: './pics/ticha.jpeg',
-        name:     'Ticha Godwill',
-        position: 'Diaspora president',
-        email:    'infostichagodwill@gmail.com',
-    
-     },
-     {
-        id: 2, 
-        image: './pics/etiene.jpeg',
-        name:     'Awa Ettiene',
-        position: 'Ground Zero president',
-        email:    'awa@gmail.com',
-       
-        
-     },
-     {
-        id: 3,
-        image: './pics/asahna.jpeg',
-        name:     'Asahna Fonkam',
-        position: 'Sec General',
-        email:    'asahna@gmail.com',
-       
-     },
-     {
-        id: 4,
-        image: './pics/kiyang.jpeg',
-        name:     'Kiyang Rene',
-        position: 'V. president',
-        email:    'kiyang@gmail.com',
-        
-      
-     },
-     {
-        id: 5,
-        image: './pics/victorrykhan.jpeg',
-        name:     'Khan Victory ',
-        position: 'Fin Sec',
-        email:    'khan@gmail.com',
-  
-     },
-     {
-        id: 6,
-        image: './pics/penny.jpeg',
-        name:     'Teneng Elvis',
-        position: 'Assistant PRO',
-        email:    'teneng@gmail.com',
-     },
-     {
-        id: 7,
-        image: './pics/jude.jpeg',
-        name:     'Mokom Jude',
-        position: 'PRO',
-        email:    'mokom@gmail.com',
-        
-     },
-     {
-        id: 8,
-        image: './pics/mandela.jpeg',
-        name:     'Cho Nicholas',
-        position: 'Technical Adviser',
-        email:    'cho@gmail.com',
-     
-     },
-     {
-        id: 9,
-        image: './pics/anyamclaudete.jpeg',
-        name:     'Anyam Claudette',
-        position: 'Assistant SG',
-        email:    'anyam@gmail.com',
-  
-     },
-     {
-     id: 10,
-     image: './pics/meshinfor.jpeg',
-     name:     'Penn Claudette',
-     position: 'Unknow',
-     email:    'penn@gmail.com',
+export const members = [
+  {
+    id: 1,
+    image: process.env.PUBLIC_URL + "/pics/ticha.jpeg",
+    name: "Ticha Godwill",
+    position: "Diaspora president",
+    email: "infostichagodwill@gmail.com",
   },
   {
-   id: 11,
-   image: './pics/ndah.jpeg',
-   name:     'Promise Ndah',
-   position: 'Treasurer',
-   email:    'penn@gmail.com',
-},
-{
-   id: 12,
-   image: './pics/joyce.jpeg',
-   name:     'Wondighe Joyce',
-   position: 'Unknow',
-   email:    'penn@gmail.com',
-},
-{
-   id: 13,
-   image: './pics/awadesmond.jpeg',
-   name:     'Awa Desmond',
-   position: 'DM',
-   email:    'penn@gmail.com',
-},
-
-
-]
+    id: 2,
+    image: process.env.PUBLIC_URL + "/pics/etiene.jpeg",
+    name: "Awa Ettiene",
+    position: "Ground Zero president",
+    email: "awa@gmail.com",
+  },
+  {
+    id: 3,
+    image: process.env.PUBLIC_URL + "/pics/asahna.jpeg",
+    name: "Asahna Fonkam",
+    position: "Sec General",
+    email: "asahna@gmail.com",
+  },
+  {
+    id: 4,
+    image: process.env.PUBLIC_URL + "/pics/kiyang.jpeg",
+    name: "Kiyang Rene",
+    position: "V. president",
+    email: "kiyang@gmail.com",
+  },
+  {
+    id: 5,
+    image: process.env.PUBLIC_URL + "/pics/victorrykhan.jpeg",
+    name: "Khan Victory ",
+    position: "Fin Sec",
+    email: "khan@gmail.com",
+  },
+  {
+    id: 6,
+    image: process.env.PUBLIC_URL + "/pics/penny.jpeg",
+    name: "Teneng Elvis",
+    position: "Assistant PRO",
+    email: "teneng@gmail.com",
+  },
+  {
+    id: 7,
+    image: process.env.PUBLIC_URL + "/pics/jude.jpeg",
+    name: "Mokom Jude",
+    position: "PRO",
+    email: "mokom@gmail.com",
+  },
+  {
+    id: 8,
+    image: process.env.PUBLIC_URL + "/pics/mandela.jpeg",
+    name: "Cho Nicholas",
+    position: "Technical Adviser",
+    email: "cho@gmail.com",
+  },
+  {
+    id: 9,
+    image: process.env.PUBLIC_URL + "/pics/anyamclaudete.jpeg",
+    name: "Anyam Claudette",
+    position: "Assistant SG",
+    email: "anyam@gmail.com",
+  },
+  {
+    id: 10,
+    image: process.env.PUBLIC_URL + "/pics/meshinfor.jpeg",
+    name: "Penn Claudette",
+    position: "Unknow",
+    email: "penn@gmail.com",
+  },
+  {
+    id: 11,
+    image: process.env.PUBLIC_URL + "/pics/ndah.jpeg",
+    name: "Promise Ndah",
+    position: "Treasurer",
+    email: "penn@gmail.com",
+  },
+  {
+    id: 12,
+    image: process.env.PUBLIC_URL + "/pics/joyce.jpeg",
+    name: "Wondighe Joyce",
+    position: "Unknow",
+    email: "penn@gmail.com",
+  },
+  {
+    id: 13,
+    image: process.env.PUBLIC_URL + "/pics/awadesmond.jpeg",
+    name: "Awa Desmond",
+    position: "DM",
+    email: "penn@gmail.com",
+  },
+];
